35d621fc9ce12952acdaac020de32faed45d3e03
AndykingSkywalker <andrewk640@gmail.com>
2021-03-04 19:07:59 +0000

    random number generator added

--- Mixed_Messages.js
+++ Mixed_Messages.js
@@ -1,1 +1,5 @@
 // This project will provide the user with a randomly generated message everytime they execute the code
+
+const randomGeneratedNumber = num => {
+    return Math.floor(Math.random() * num)
+}
