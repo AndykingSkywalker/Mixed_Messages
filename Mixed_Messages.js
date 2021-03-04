b64d83250c183df0acbba5a6c42d3c995e301cc2
AndykingSkywalker <andrewk640@gmail.com>
2021-03-04 19:21:24 +0000

    motivation factory and empty array to store

--- Mixed_Messages.js
+++ Mixed_Messages.js
@@ -3,3 +3,11 @@
 const randomGeneratedNumber = num => {
     return Math.floor(Math.random() * num)
 }
+
+const collectiveMotivation = {
+    mood: ['motivated', 'excited', 'ambitious', 'determined'],
+    motivationalPhrase: ['You can do this', 'You\'re the best', 'Do not give up!', 'Show them what you\'re made of', 'Never back down!', 'Keep Trying!'],
+    advice: ['Take it easy', 'Keep pushing yourself', 'Just do it', 'Be optimistic!']
+}
+
+let personalMotivation = []
