8cbc8ce565880180ca692fbdbe98f6a0eb201851
Andrew King <78704852+AndykingSkywalker@users.noreply.github.com>
2021-03-04 19:54:24 +0000

    Update Mixed_Messages.js

--- Mixed_Messages.js
+++ Mixed_Messages.js
@@ -6,7 +6,7 @@ const randomGeneratedNumber = num => {
 
 const collectiveMotivation = {
     mood: ['motivated', 'excited', 'ambitious', 'determined'],
-    motivationalPhrase: ['You can do this', 'You\'re the best', 'Do not give up!', 'Show them what you\'re made of', 'Never back down!', 'Keep Trying!'],
+    motivationalPhrase: ['You can do this', 'You\'re the best', 'Don\'t give up!', 'Show them what you\'re made of', 'Never back down!', 'Keep Trying!'],
     advice: ['Take it easy', 'Keep pushing yourself', 'Just do it', 'Be optimistic!']
 }
 
